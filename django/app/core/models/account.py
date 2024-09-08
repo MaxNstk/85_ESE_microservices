da75c1f5c522bc13f22b1ce735db5417d5977453
Max Natanael Starke <max.starke@singular.inf.br>
2024-09-08 14:01:25 -0300

    feat: criando categoria

--- django/app/core/models/account.py
+++ django/app/core/models/account.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.conf import settings
+from .base_model import BaseModel
 
 
 class AccountType(models.TextChoices):
     CHECKING = "CHECKING", "Corrente"
     SAVINGS = "SAVINGS", "Poupança"
 
-class Account(models.Model):
+
+class Account(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, verbose_name='Usuário')
     bank_name = models.CharField(max_length=50, verbose_name='Nome do Banco')
     account_number = models.CharField(max_length=20, verbose_name='Número da Conta', unique=True)
     account_type = models.CharField(max_length=10, choices=AccountType.choices, default=AccountType.CHECKING, verbose_name='Tipo de Conta')
     branch_code = models.CharField(max_length=10, blank=True, null=True, verbose_name='Código da Agência')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de Criação')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Data de Atualização')
     
     def __str__(self):
         return f"({self.user.username}) {self.bank_name} - {self.account_number}"
