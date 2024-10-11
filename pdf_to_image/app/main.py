2fc9ac7c944b29e3952c6411789abc7076c149a9
Max Natanael Starke <max.starke@singular.inf.br>
2024-10-10 21:06:26 -0300

    refactor: remoção prefixo /api no fast api

--- pdf_to_image/app/main.py
+++ pdf_to_image/app/main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 from app.api.v1.upload_pdf import router as upload_router
 
 app = FastAPI()
-app.include_router(upload_router, prefix="/api/v1")
+app.include_router(upload_router, prefix="/v1")
